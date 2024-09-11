8b5126623d785321cac01d3c2ef0a937d000bed0
banghyunwoo <brandon55321@gmail.com>
2024-09-11 15:26:26 +0900

    refactor: 커피가게 완성
    ArrayList로 구현. User클래스 싱글톤으로 표현해야하는 문제 존재

---
+++ src/main/java/org/cafe/Order.java
@@ -0,0 +1,92 @@
+package org.cafe;
+
+import java.util.Scanner;
+
+public class Order implements OrderSystem {
+    String name;
+    int price;
+    int count = 0;
+    User user = new User();
+    KioskSystem kioskSystem = new KioskSystem();
+
+    @Override
+    public void order() {
+        int number = kioskSystem.askNumber();
+        switch (number) {
+            case 1 -> {
+                name = "아메리카노";
+                price = 3800;
+            }
+            case 2 -> {
+                name = "에스프레소";
+                price = 2400;
+            }
+            case 3 -> {
+                name = "카페라떼";
+                price = 4200;
+            }
+            case 4 -> {
+                name = "밀크티";
+                price = 5100;
+            }
+            default -> {
+                kioskSystem.faultValue();
+                count--;
+            }
+
+        }
+        count++;
+        User.orders.add(count - 1, name);
+        System.out.println("현재 주문량: " + count);
+        user.total += price;
+    }
+
+    @Override
+    public void cancel() {
+        System.out.println(User.orders);
+        System.out.println("삭제할 메뉴번호를 적어주세요.");
+        int number = kioskSystem.askNumber();
+        //넘버, 빠지는 글자와 일치하는거 빼고, 가격도 뺀다.
+        String remove = User.orders.remove(number -1);
+        switch (remove) {
+            case "아메리카노" -> {
+                user.total -= 3800;
+            }
+            case "에스프레소" -> {
+                user.total -= 2400;
+            }
+            case "카페라떼" -> {
+                user.total -= 4200;
+            }
+            case "밀크티" -> {
+                user.total -= 5100;
+            }
+        }
+        count--;
+    }
+
+    @Override
+    public void pay() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(user.total + "원");
+        System.out.println("지불할 금액을 입력하세요.");
+        int paid = scanner.nextInt();
+        if (paid < user.total) {
+            System.out.println("금액이 부족합니다");
+            pay();
+        } else {
+            System.out.println("잔돈: " + (paid - user.total) + "원");
+        }
+        user.total = 0;
+        User.orders.clear();
+    }
+
+    public void close() {
+        kioskSystem.end();
+        System.exit(0);
+    }
+
+    public void faultNumber() {
+        System.out.println("잘못된 번호 입력입니다.");
+    }
+}
---
+++ src/main/java/org/cafe/User.java
@@ -0,0 +1,13 @@
+package org.cafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    int total = 0;
+
+    static List<String> orders = new ArrayList<>();
+    //TODO 싱글톤 패턴으로 만들어보기
+
+}
